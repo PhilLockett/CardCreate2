ab6dd6a67d11be036b9cffb3945f9d76f54de45e
Phil Lockett <phillockett65@gmail.com>
2022-03-16 14:52:14 -0400

    Fixed copyright.

--- src/main/java/phillockett65/CardCreate2/App.java
+++ src/main/java/phillockett65/CardCreate2/App.java
@@ -1,21 +1,21 @@
-/*  PTDesigner - a simple application to design a periodic table.
+/*  CardCreate2 - a JavaFX based playing card image generator.
  *
- *  Copyright 2020 Philip Lockett.
+ *  Copyright 2022 Philip Lockett.
  *
- *  This file is part of PTDesigner.
+ *  This file is part of CardCreate2.
  *
- *  PTDesigner is free software: you can redistribute it and/or modify
+ *  CardCreate2 is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  PTDesigner is distributed in the hope that it will be useful,
+ *  CardCreate2 is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with PTDesigner.  If not, see <https://www.gnu.org/licenses/>.
+ *  along with CardCreate2.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /*
